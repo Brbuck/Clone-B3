8b4af304b692d8e3e0096618eeb9c84a94e2cca0
Brbuck <62665590+Brbuck@users.noreply.github.com>
2021-07-07 11:30:46 -0300

    Update in Project

---
+++ src/components/Slider/SliderData.js
@@ -0,0 +1,15 @@
+export const SliderData = [
+    {
+      image:'http://www.b3.com.br/data/files/70/05/C1/C9/7012A710858421A7AC094EA8/Banner_1440x300px_03.png',
+      title: 'Nova Área Logada Da B3',
+      conteudo: 'Confira seus investimentos de maneira unificada, com a confiança da informação direto da bolsa de valores',
+      botao: 'ACESSE'
+        
+    },
+    {
+      image:'http://www.b3.com.br/data/files/81/84/BC/F4/2DF2A710D25F12A7AC094EA8/b3_21-074_ipo-br-partners_banner.gif',
+      title: 'BRBI11',
+      conteudo: 'Início da negociação das ações do Br Partners',
+      botao: 'CONFIRA A TRANSMISSÃO'
+    }
+  ];
